72d2059dc9b87b455ed1c1bed6a3a301458f2e12
Caesar Chi <caesar@mitac.com.tw>
2012-11-06 16:42:20 +0800

    update cli, and library

--- bin/index.js
+++ bin/index.js
@@ -1,21 +1,74 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var ydl = require('../index');
 var version = '0.0.1';
+
+function ydlErrorHandler (url) {
+    // youtube download error happen
+    ydl.error(function () {
+      console.log('[STATUS] FAIL Fetch URL : ' + env);
+    });
+}
 
 program
 .version(version)
-.option('-u, --url <url>', 'Add YouTube url link')
-.option('-h, --help', 'Help commnand line')
-.parse(process.argv);
-
+.option('--url <url>', 'Give me the YouTube URL, Fetch YouTube video')
+.option('--mp3 <url>', 'Give me the YouTube URL, Convert Video to MP3 file')
 
 program.on('--help', function(){
   console.log('  Examples:');
   console.log('');
-  console.log('ydl url', 'Add YouTube url link');
+  console.log('    ydl http://www.youtube.com/watch?v=9bZkp7q19f0', ' Fetching the GANGNAM Style Music Video');
   console.log('');
-  console.log('version: ', version);
+  console.log('  version: ', version);
 });
-//var ydl = require('../index');
-console.log(program.url);
+
+program
+.command('*')
+.description('Add YouTube URL, default will convert video to mp3')
+.action(function(env){
+    console.log('[STATUS] Fetch URL : ' + env);
+
+    // youtube download error happen
+    ydlErrorHandler(env);
+
+    // youtube download execute
+    ydl.exec(env, 'mp3', function () {
+      console.log('[STATUS] Success fetch URL : ' + env);
+      console.log('[STATUS] Convert file to mp3 type');
+    });
+});
+
+program.parse(process.argv);
+
+if (program.mp3) {
+  console.log('Start fetching URL...');
+  console.log('[MODE] MP3 mode');
+  var env = program.url;
+
+  // youtube download error happen
+  ydlErrorHandler(env);
+
+  // youtube download execute
+  ydl.exec(env, 'mp3', function () {
+    console.log('[STATUS] Success fetch URL : ' + env);
+    console.log('[STATUS] Convert file to mp3 type');
+  });
+}
+
+// normal fetch file version
+if (program.url) {
+  console.log('Start fetching URL...');
+  console.log('[MODE] Normal mode');
+  var env = program.url;
+
+  // youtube download error happen
+  ydlErrorHandler(env);
+
+  // youtube download execute
+  ydl.exec(env, 'flv', function () {
+    console.log('[STATUS] Success fetch URL : ' + env);
+    console.log('[STATUS] Finish');
+  });
+}
